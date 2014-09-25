0152aa0015734c32d3dfc8092c7428fd1818a78b
elliotlewis8923 <elewis8923@gmail.com>
2014-09-25 15:58:16 +0100

    initialised express skeleton, test files with mocha, chai, zombie, passed first test

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var server = require('../app');
+var chai = require('chai');
+var expect = chai.expect;
+var Browser = require('zombie')
+
+describe('Homepage', function() {
+	var browser;
+
+	before(function() {
+		server = server.listen(3000);
+		browser = new Browser()
+		
+	});
+
+	it('should display a header', function() {
+		browser.visit("http://localhost:3000/", function() {
+			expect(browser.text('header')).to.eql('The Circuit');
+		});
+	});
+
+
+});
